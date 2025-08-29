e7c288fa203afb9c780501f2346a1fdcbc06afc3
ISHA TYAGI <isha.2428cseaiml277@kiet.edu>
2025-08-29 11:41:53 +0530

    29 August 2

---
+++ Bank.java
@@ -0,0 +1,13 @@
+public class Bank {
+    private int balance = 10000;
+    public void withdraw(int amount){
+        if(amount>balance){
+            throw new InsufficientBalanceException("Cannot withdraw amount greater than balance");
+        }
+        else{
+            balance = balance - amount;
+            System.out.println("Withdraw Successful, Remaining balance "+balance);
+        }
+
+    }
+}
---
+++ Mybank.java
@@ -0,0 +1,6 @@
+public class Mybank {
+    public static void main(String[] args) {
+        Bank obj = new Bank();
+        obj.withdraw(2000);
+    }
+}
